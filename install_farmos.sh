50c5af6c9b4940f7959c1160dc7c80160f14da82
Henry Robert Muwanika <hrmuwanika@gmail.com>
2020-07-12 10:58:43 +0200

    Update install_farmos.sh

--- install_farmos.sh
+++ install_farmos.sh
@@ -44,12 +44,14 @@ sudo systemctl start docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+#----------------------------------------------------------------------------------------
+# FarmOS Development
+#----------------------------------------------------------------------------------------
+cd /usr/src
 sudo mkdir farmOS
 cd farmOS
 wget https://raw.githubusercontent.com/farmOS/farmOS/7.x-1.x/docker/docker-compose.development.yml
-wget https://raw.githubusercontent.com/farmOS/farmOS/7.x-1.x/docker/docker-compose.production.yml
 sudo cp docker-compose.development.yml docker-compose.yml
-#sudo cp docker-compose.production.yml docker-compose.yml
 sudo docker-compose up -d
 
 #----------------------------------------------------------------------------------------
@@ -60,3 +62,12 @@ echo "Database username: farm"
 echo "Database password: farm"
 echo "Under "Advanced options", change "Database host" to: db"
 #----------------------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------------------
+# FarmOS Production
+#----------------------------------------------------------------------------------------
+# sudo docker-compose down
+# cd /usr/src/farmOS
+# wget https://raw.githubusercontent.com/farmOS/farmOS/7.x-1.x/docker/docker-compose.production.yml
+# sudo cp docker-compose.production.yml docker-compose.yml
+# sudo docker-compose up -d
