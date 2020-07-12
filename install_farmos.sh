f9f45f2dd785a037708f82996c4016c1dca668d3
Henry Robert Muwanika <hrmuwanika@gmail.com>
2020-07-12 10:08:02 +0200

    Update install_farmos.sh

--- install_farmos.sh
+++ install_farmos.sh
@@ -32,10 +32,7 @@ sudo apt autoremove -y
 #--------------------------------------------------
 ### Installation of Docker
 #---------------------------------------------------
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
+sudo apt remove docker docker-engine docker.io
 sudo apt install -y docker.io
 
 sudo systemctl enable docker
