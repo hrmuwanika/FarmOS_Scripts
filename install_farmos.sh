27b2af75900e6c9ed817b78576c1588cb72f97ee
Henry Robert Muwanika <hrmuwanika@gmail.com>
2020-07-12 10:01:57 +0200

    Update install_farmos.sh

--- install_farmos.sh
+++ install_farmos.sh
@@ -36,7 +36,7 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
-sudo apt install -y docker-ce 
+sudo apt install -y docker.io
 
 sudo systemctl enable docker
 sudo systemctl start docker
