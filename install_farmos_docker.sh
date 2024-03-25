c5ecc740f25ef2656c941bcadf470be955186281
Henry Robert Muwanika <hrmuwanika@gmail.com>
2024-03-25 02:11:41 +0200

    Update install_farmos_docker.sh

--- install_farmos_docker.sh
+++ install_farmos_docker.sh
@@ -35,7 +35,7 @@ sudo apt autoremove -y
 # Remove older versions of docker
 sudo apt remove -y docker docker-engine docker.io containerd runc
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
@@ -47,10 +47,10 @@ sudo systemctl start docker && sudo systemctl enable docker
 #----------------------------------------------------------------------------------------
 # Installation of Docker compose
 #----------------------------------------------------------------------------------------
-mkdir -p ~/.docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-chmod +x ~/.docker/cli-plugins/docker-compose
-
+# mkdir -p ~/.docker/cli-plugins/
+# curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+# chmod +x ~/.docker/cli-plugins/docker-compose
+sudo apt install -y docker-compose
 #----------------------------------------------------------------------------------------
 # FarmOS Production
 #----------------------------------------------------------------------------------------
